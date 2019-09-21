3d0f62bbc65a5f9b556402e58435e4210d62b6d9
Paul Battley <pbattley@gmail.com>
2019-09-21 16:12:27 +0100

    Add script to generate JSON dictionary

---
+++ dictionary.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+root = {}
+
+ARGF.each_line do |word|
+  word.chomp!
+  next if word.length < 3
+  node = root
+  word.scan(/QU|./).each do |letter|
+    node = (node[letter] ||= {})
+  end
+  node['.'] = 1
+end
+
+puts JSON.pretty_generate(root)
